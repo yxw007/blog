34aa5fbb95e5b74dfb35a66fa0863889d2e7e021
Potter <75838259@qq.com>
2024-07-03 10:44:09 +0800

    chore: update dependencies and add Giscus for comments

--- docs/.vitepress/theme/layout/Layout.vue
+++ docs/.vitepress/theme/layout/Layout.vue
@@ -1,25 +1,31 @@
 <template>
 	<Layout>
 		<template #doc-after>
-			<!-- 	<Comment
-				v-if="
-					(theme.commentConfig?.showComment ?? true) &&
-					(frontmatter?.showComment ?? true)
-				"
-				:commentConfig="theme.commentConfig"
-				:key="md5(page.relativePath)" /> -->
+			<Giscus repo="yxw007/yxw007.github.io"
+							repo-id="MDEwOlJlcG9zaXRvcnkyOTY4MTA2Mjc="
+							category="General"
+							category-id="DIC_kwDOEbD4g84CgiiK"
+							mapping="specific"
+							:term="filename"
+							strict="1"
+							reactions-enabled="1"
+							emit-metadata="0"
+							input-position="top"
+							:theme="isDark ? 'dark' : 'light'"
+							lang="en" />
 		</template>
 	</Layout>
 </template>
 
 <script setup>
 import DefaultTheme from "vitepress/theme";
-// import md5 from "md5";
+import Giscus from "@giscus/vue";
 import { useData } from "vitepress";
-// import Comment from "../components/Comment.vue";
 
 const { Layout } = DefaultTheme;
-const { page, theme, frontmatter } = useData();
+const { page, isDark } = useData();
+const relativePath = page.value.relativePath;
+const filename = relativePath.substring(0, relativePath.lastIndexOf('.'));
 </script>
 
 <style scoped></style>
